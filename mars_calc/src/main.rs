bdcff579659f74082fa5f7a89a1cb16815224412
HyunSang Park <me@hyunsang.dev>
2023-05-16 12:30:54 +0900

    feat: update mutalilty

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // 러스트 모든 변수는 불변 변수임. 변수 값을 할당 되었다면 절대로 변경이 불가능함.
+    // 가변 변수로 바꾸기 위해서는 선언한 후 `mut`를 사용하면 됨.
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    mars_weight = mars_weight * 1000.0;
+    println!("Weight on Mars: {}kg", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32{
+    (weight / 9.81) * 3.711
 }
