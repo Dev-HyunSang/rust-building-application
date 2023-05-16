a6fdf434ee9633070c3aa8db06f9b36ce60452d3
HyunSang Park <me@hyunsang.dev>
2023-05-16 12:43:01 +0900

    feat: update 표준 라이브러리

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,4 +1,9 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
     // 러스트 모든 변수는 불변 변수임. 변수 값을 할당 되었다면 절대로 변경이 불가능함.
     // 가변 변수로 바꾸기 위해서는 선언한 후 `mut`를 사용하면 됨.
     let mut mars_weight = calculate_weight_on_mars(100.0);
